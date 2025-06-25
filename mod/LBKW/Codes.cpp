f462ca3746360552bcbbb7a9a0d9a7d2257ddcc1
Bebja <148148114+Bebja@users.noreply.github.com>
2025-06-25 19:56:46 +0200

    Update and rename LBKWcodes.cpp to Codes.cpp

---
+++ mod/LBKW/Codes.cpp
@@ -0,0 +1,18 @@
+#include <kamek.hpp>
+#include <MarioKartWii/Race/RaceData.hpp>
+#include <Settings/UI/SettingsPanel.hpp>
+#include <LBKW.hpp>
+
+namespace LBKW {
+namespace Codes {
+
+// KmWrite Codes
+
+// C2 Codes
+
+// Toggle KmWrites
+
+// Toggle C2s
+
+} // namespace Codes
+} // namespace LBKW
