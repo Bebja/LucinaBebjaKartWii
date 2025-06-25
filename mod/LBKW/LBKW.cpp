f2ec208d44f35442384d29034c835b9b03dbf580
Bebja <148148114+Bebja@users.noreply.github.com>
2025-06-25 19:52:42 +0200

    Update LBKW.cpp
    add something here, basically copying from BKW2, just renaming

--- mod/LBKW/LBKW.cpp
+++ mod/LBKW/LBKW.cpp
@@ -1,1 +1,25 @@
 
+#include <MarioKartWii/Race/RaceData.hpp>
+#include <Settings/UI/SettingsPanel.hpp>
+#include <SlotExpansion/CupsConfig.hpp>
+#include <LBKW.hpp>
+
+namespace LBKW {
+
+Pulsar::System *LBKWPack::Create() {
+	return new LBKWPack(); //now Pulsar::sInstance is of type MyPack
+}
+Pulsar::System::Inherit CreateLBKW(LBKWPack::Create); //Create a Inherit that'll get called back by Pulsar::CreatePulsar
+
+void LBKWPack::AfterInit() {
+	++Pulsar::UI::SettingsPanel::pageCount;
+
+	// Radio button count for new page
+	Pulsar::UI::SettingsPanel::radioButtonCount[SETTINGSTYPE_LBKW] = 1;
+
+}
+
+//Pulsar::Ghost::Manager also has a callback which can be set using SetCb
+//That callback will be called as each ghost is loaded
+
+} // namespace BKW
