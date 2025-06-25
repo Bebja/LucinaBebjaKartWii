676356af69d2189499d33abd64b73b796062d844
Bebja <148148114+Bebja@users.noreply.github.com>
2025-06-25 19:53:23 +0200

    Update LBKW.hpp

--- mod/LBKW/LBKW.hpp
+++ mod/LBKW/LBKW.hpp
@@ -1,1 +1,15 @@
+#pragma once
 
+#include <PulsarSystem.hpp>
+#include <LBKWCommon.hpp>
+
+namespace LBKW {
+	
+class LBKWPack : public Pulsar::System {
+public:
+    static Pulsar::System *Create(); //My Create function, needs to return a ptr to a Pulsar::System
+    static LBKWPack *GetInstance() { return static_cast<BKWPack*>(sInstance); } //for ease of use, optional
+	void AfterInit() override;
+};
+
+} // namespace LBKW
